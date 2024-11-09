3ae681916cec575f574da0f86ea94640a96c0c1f
Denislav Tsenov <150596050+Denkata7622@users.noreply.github.com>
2024-11-09 16:18:58 +0200

    UrlShortener 1.22

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -9,7 +9,8 @@ const shortUrlSchema = new mongoose.Schema({
     short: {
         type: String,
         required: true,
-        default: shortId.generate
+        // If a custom short URL is provided, use that; otherwise, generate a short one
+        default: function() {}
     },
     clicks: {
         type: Number,
